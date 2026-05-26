ffb790e762dcc85db41a2135e37a4dc470eefeac
Shailesh <sy2110697@gmail.com>
2026-05-26 17:40:34 +0530

    updated the admission.model.js

--- Backend/src/model/admission.model.js
+++ Backend/src/model/admission.model.js
@@ -2,20 +2,69 @@ import mongoose from "mongoose";
 
 const admissionSchema = new mongoose.Schema(
   {
-    fullName: String,
-    phone: String,
-    age: String,
-    course: String,
-    height: String,
-    weight: String,
-    dob: String,
-    qualification: String,
-    caste: String,
-    address: String,
+    fullName: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    age: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    course: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    height: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    weight: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    dob: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    qualification: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    caste: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    address: {
+      type: String,
+      default: "",
+      trim: true,
+    },
   },
   { timestamps: true }
 );
 
 const Admission = mongoose.model("Admission", admissionSchema);
 
-export default Admission;   
+export default Admission;
