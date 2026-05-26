3fa514468018e226e9f1ec5b80bf83a9d97061af
Shailesh <sy2110697@gmail.com>
2026-05-26 16:29:57 +0530

    fix google sheet export

--- Backend/src/services/googleSheets.service.js
+++ Backend/src/services/googleSheets.service.js
@@ -1,14 +1,34 @@
 import axios from "axios";
 
-const SHEET_ID = process.env.GOOGLE_SHEET_ID;
-const API_KEY = process.env.GOOGLE_API_KEY;
+export const fetchGoogleSheetData = async () => {
+  try {
+    const SHEET_ID = process.env.GOOGLE_SHEET_ID;
+    const API_KEY = process.env.GOOGLE_API_KEY;
 
-export const fetchSheetData = async () => {
-  const range = "Form Responses 1!A:D";
+    const RANGE = "Form Responses 1!A:F";
 
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${range}?key=${API_KEY}`;
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`;
 
-  const response = await axios.get(url);
+    const response = await axios.get(url);
 
-  return response.data.values;
+    const rows = response.data.values;
+
+    if (!rows || rows.length <= 1) {
+      return [];
+    }
+
+    const formattedData = rows.slice(1).map((row) => ({
+      timestamp: row[0],
+      fullName: row[1],
+      phone: row[2],
+      age: row[3],
+      course: row[4],
+      address: row[5],
+    }));
+
+    return formattedData;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 };
