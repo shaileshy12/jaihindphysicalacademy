cebfa5ab85536cff2fc1b3d3965c25b1d78d492a
Shailesh <sy2110697@gmail.com>
2026-06-05 18:31:15 +0530

    Fix Railway trust proxy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,6 +15,8 @@ import helmet from "helmet";
 
 // const URL = process.env.CLIENT_URL;
 const app = express();
+
+app.set("trust proxy", 1);
 // app.use(Sentry.Handlers.requestHandler());
 
 app.use(
